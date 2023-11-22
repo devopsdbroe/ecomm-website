891b5667cb373c7e1e1eebc833ae7de11af1c3dc
devopsdbroe <dbroe1995@gmail.com>
2023-11-21 20:07:56 -0500

    Implemented Banner component with Image Slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
+import Banner from "@/components/Banner";
+
 export default function Home() {
 	return (
 		<main>
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor similique
-			et beatae veritatis quidem ratione numquam qui voluptatum facilis aliquam,
-			praesentium unde aperiam, quisquam accusantium quis quae. Ex, vel est!
+			<Banner />
 		</main>
 	);
 }
